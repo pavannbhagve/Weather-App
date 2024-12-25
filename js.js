bbd54fb7fe835f9a8dc55d8f9e90456f5d879969
PB <pb@gmail.com>
2024-12-25 18:27:31 +0530

    All done with js

--- js.js
+++ js.js
@@ -1,9 +1,59 @@
 const apikey = "3b6d646bac08cdb1e28f9855d3c4a2ba";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=matric&q=delhi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
-var data = await response.json();
-    
+async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if (response.status==404){
+        
+        document.querySelector(".weather").style.display = "none";
+        
+    }else {
+
+
+        var data = await response.json();
+
+document.querySelector(".city").innerHTML = data.name;
+document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°C";
+document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+document.querySelector(".wind").innerHTML = data.wind.speed + "Km/hr"; 
+
+        if(data.weather[0].main == "Clouds"){
+
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if (data.weather[0].main  == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        
+        }else if (data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+            
+        }else if (data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+            else if (data.weather[0].main == "Mist"){
+                weatherIcon.src = "images/mist.png";
+            }
+        
+        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".error").style.display ="none"
+
+    }
+
+
+
+
+
 }
+
+searchButton.addEventListener("click",()=>{
+
+    checkWeather(searchBox.value);
+
+})
+
