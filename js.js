c7c463ae06005bade705f828389e7b49b60c13ee
PB <pb@gmail.com>
2024-12-25 17:33:14 +0530

    Api add in js for fetch

--- js.js
+++ js.js
@@ -1,2 +1,9 @@
 const apikey = "3b6d646bac08cdb1e28f9855d3c4a2ba";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=matric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=matric&q=delhi";
+
+async function checkWeather() {
+
+    const response = await fetch(apiUrl + `&appid=${apikey}`);
+var data = await response.json();
+    
+}
